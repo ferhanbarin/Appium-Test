8babc59a67c72cdfee87507e0b7fee9a3f8cd26d
Ferhan BARIN <ferhanbarin@yahoo.com>
2022-02-23 11:33:31 +0200

    Appium

--- src/test/java/setups/Capabilities.java
+++ src/test/java/setups/Capabilities.java
@@ -1,24 +1,18 @@
 package setups;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.io.File;
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 public class Capabilities extends DesiredCapabilities {
+
     protected AndroidDriver driver;
     private AppiumDriverLocalService service;
 
     protected void preparation() throws Exception {
-        File app = new File("/Users/Q1/Desktop/MyPc/projeler/QA/foreskTrader.apk.zip");
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
@@ -27,7 +21,6 @@ public class Capabilities extends DesiredCapabilities {
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
         capabilities.setCapability("appium:appPackage", "foreks.android");
         capabilities.setCapability("appium:appActivity", "foreks.android.SplashActivity");
-        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
 //        capabilities.setCapability("wdaStartupRetries", "8");
 //        capabilities.setCapability("wdaStartupRetryInterval", "20000");
 
